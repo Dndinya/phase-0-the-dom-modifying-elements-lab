0a7ce8b5aae2d8e659ef32e54dbc22bf6cd15da4
Derrick <derrick.ndinya@student.moringaschool.com>
2023-12-21 16:04:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "DERRICK is the champion";
+document.body.append(newHeader);
